cc12982102312368e3e190c3f296a5bdf0b508d3
maddyb99 <43785086+maddyb99@users.noreply.github.com>
2020-09-12 20:15:21 +0530

    add api key auth

--- functions/src/v1/main.ts
+++ functions/src/v1/main.ts
@@ -1,5 +1,6 @@
 import * as express from "express";
 import * as utils from "./utils";
+import * as functions from "firebase-functions";
 import { userModule } from "./user";
 import { postModule } from "./posts";
 
@@ -9,6 +10,16 @@ const version = "/api/v1";
 const userApi = utils.registerModule("/user", userModule);
 const postApi = utils.registerModule("/post", postModule);
 
+const apiKey = functions.config().pratipali.client_key;
+const authMiddleware = function (req: any, res: any, next: any) {
+  if (req.headers.authorization !== apiKey) {
+    return res.status(403).json({ error: "Unauthorized request!" });
+  }
+  next();
+  return null;
+};
+
+apiInternal.use(authMiddleware);
 apiInternal.use(version, userApi);
 apiInternal.use(version, postApi);
 
